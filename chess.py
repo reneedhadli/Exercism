d057edc6e3dabc6116d3b6f32c7f00fd61101d4a
Renee Dhadli <reneedhadli250@gmail.com>
2026-06-04 22:04:56 +0530

    Refactor total function loop variable name
    Updated the total function to use 'loop' instead of 'i'.

--- chess.py
+++ chess.py
@@ -1,12 +1,18 @@
+"""Calculate the number of grains of wheat on a chessboard.
+ 
+A chessboard has 64 squares. Square 1 has one grain, square 2 has two grains, square 3 has four grains, and so on, doubling each time.
+ 
+This code calculates:
+ 
+the number of grains on a given square
+the total number of grains on the chessboard
+"""
 def square(number):
     if number < 1 or number > 64:
         raise ValueError("square must be between 1 and 64")
     return 2 ** (number-1)
-
-
 def total():
     result = 0
-    for i in range(1, 65):
-        result = result + square(i)
+    for loop in range(1, 65):
+        result = result + square(loop)
     return result
-
