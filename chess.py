dfc4bf7be6046f40a87e28404c163e0e347e2c62
Renee Dhadli <reneedhadli250@gmail.com>
2026-06-04 21:48:35 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,12 @@
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number-1)
+
+
+def total():
+    result = 0
+    for i in range(1, 65):
+        result = result + square(i)
+    return result
+
